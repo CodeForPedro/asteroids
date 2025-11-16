bf76f4183b5f7fce66ae623880d1bf639e0843bb
CodeForPedro <codeforpedro@proton.me>
2025-11-16 15:08:33 -0500

    Create constants module, import screen dimensions to main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+
+
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver} \nScreen width: 1280 \nScreen height: 720")
 
 
 if __name__ == "__main__":
