3383736b9c3ff67908e2b9f3db51c22334b290df
CodeForPedro <codeforpedro@proton.me>
2025-11-16 16:48:47 -0500

    Add pygame screen and close function

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
-
+from logger import log_state
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} \nScreen width: 1280 \nScreen height: 720")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
