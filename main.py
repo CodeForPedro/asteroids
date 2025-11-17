8042f431b30ab399cb84c67628420a380650c6b8
CodeForPedro <codeforpedro@proton.me>
2025-11-16 22:07:02 -0500

    Add player.py, draw triangle on screen

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} \nScreen width: 1280 \nScreen height: 720")
@@ -13,6 +13,8 @@ def main():
     clock = pygame.time.Clock()
     dt = 0 
     
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     # Game Loop
     while True:
         log_state() # This just lets just logs the state of the game
@@ -20,10 +22,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip() # This is what updates the display
         # Below method pauses the game loop until 1/60th of a second has passed, limiting to 60fps.
         dt = clock.tick(60)/1000
-
 
 if __name__ == "__main__":
     main()
