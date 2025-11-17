2048876ffd3720f8cb25de6fd1c778358b19b1f3
CodeForPedro <codeforpedro@proton.me>
2025-11-17 00:11:12 -0500

    Create class groups updatabele/drawable

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from constants import SCREEN_HEIGHT
 from logger import log_state
 from player import Player
 
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} \nScreen width: 1280 \nScreen height: 720")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,6 +17,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0 
     
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     # Game Loop
@@ -22,11 +27,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip() # This is what updates the display
         # Below method pauses the game loop until 1/60th of a second has passed, limiting to 60fps.
         dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
